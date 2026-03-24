507d73d758f121a8f70599a776359cc0dc4b0499
Christopher Wiggins <cwiggins0217@protonmail.com>
2026-03-24 13:41:28 -0500

    add data file functionality

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import "github.com/cwiggins0217/go-todo/internal"
 
 func main() {
 	todos := internal.Todos{}
+	storage := internal.NewStorage[internal.Todos]("todos.json")
+	storage.Load(&todos)
 	todos.Add("buy milk", "buy me some milk")
 	todos.Add("buy bread", "buy me some bread")
 	todos.Toggle(0)
 	todos.Print()
+	storage.Save(todos)
 }
